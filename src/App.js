862870d15f3e83041362f6b74cf9392e732e626e
anuzza <anuja3580@gmail.com>
2022-06-20 06:42:19 -0500

    alternate

--- src/App.js
+++ src/App.js
@@ -46,6 +46,19 @@ function App() {
       setIsLoading(false);
   }
 
+  let content = <p>Found no movies.</p>;
+
+  if(movies.length >0){
+    content = <MoviesList movies={movies} />;
+  }
+
+  if(error){
+    content = <p>{error}</p>;
+  }
+
+  if(isLoading){
+    content = <p>Loading...</p>;
+  }
     
     
 
@@ -55,11 +68,12 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading && movies.length >0 &&<MoviesList movies={movies} />}
+        {/* {!isLoading && movies.length >0 &&<MoviesList movies={movies} />}
         {!isLoading && movies.length === 0 && !error && <p>Found no movies.</p>}
         {isLoading && <p>Loading...</p>}
         {!isLoading && error && <p>{error}</p>}
-        
+         */}
+         {content}
       </section>
     </React.Fragment>
   );
