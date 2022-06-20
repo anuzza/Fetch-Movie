b82b5d9ef421a430019d7bffd9078035eacdfe8d
anuzza <anuja3580@gmail.com>
2022-06-20 07:00:02 -0500

    useEffect for requests

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -8,12 +8,13 @@ function App() {
   const[isLoading, setIsLoading]= useState(false);
   const [error, setError] = useState(null);
   
-  async function fetchMoviesHandler(){
+  
+   const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
 
     try{
-      const response = await fetch('https://swapi.dev/api/films');
+      const response = await fetch('https://swapi.dev/api/films/');
 
       if(!response.ok){
       throw new Error("Something went wrong!");
@@ -22,9 +23,7 @@ function App() {
     const data = await response.json();
 
     
-    // .then(response=>{ return response.json();}).then(data=>{
-
-    
+    // .then(response=>{ return response.json();}).then(data=>{    
       
       const transformedMovies= data.results.map(movieData=>{
         return {
@@ -44,7 +43,15 @@ function App() {
 
     }
       setIsLoading(false);
-  }
+  }, []);
+
+  useEffect (()=>{
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+
+  
+
 
   let content = <p>Found no movies.</p>;
 
