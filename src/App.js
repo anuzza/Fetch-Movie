dfc833e7480b37e06d2d29e17a2bbaf5af3b0d98
anuzza <anuja3580@gmail.com>
2022-06-20 23:42:48 -0500

    added a form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect, useCallback} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
+import AddMovie from './components/AddMovie';
 
 function App() {
   const [movies, setMovies]= useState([]);
@@ -49,7 +50,9 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-
+function addMovieHandler(movie){
+  console.log(movie);
+}
   
 
 
@@ -71,6 +74,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie = {addMovieHandler}/>
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
